82574d1ed214959d693d7027f34e8b1c9efa340a
Samuele Saviano <s.saviano@simultria.nl>
2024-08-06 17:02:16 +0200

    base movement

--- Assets/Scripts/Managers/EnvironmentManager.cs
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -7,18 +7,24 @@ namespace Basso.Environment
     public class EnvironmentManager : MonoBehaviour
     {
         [SerializeField] private List<EnvironmentBlock> _blocks = new List<EnvironmentBlock>();
+        [SerializeField] private float _speed;
+        [SerializeField] private float _limit;
+
+        [Space]
+        [SerializeField] private bool _update;
 
         private void Start()
         {
             EnvironmentBlock previousBlock;
             EnvironmentBlock block;
-            EnvironmentBlock nextBlock;
 
             for (int i = 0; i < _blocks.Count; i++)
             {
                 block = _blocks[i];
                 previousBlock = null;
-                nextBlock = null;
+
+                if (block == null)
+                    continue;
 
                 if (i == 0)
                 {
@@ -28,19 +34,39 @@ namespace Basso.Environment
 
                 previousBlock = _blocks[i - 1];
                 previousBlock.Next = block;
+                block.SetPosition(previousBlock);
 
                 if (i < _blocks.Count - 1) 
                 {
-                    nextBlock = _blocks[i + 1];
+                    _lastBlock = _blocks[i + 1];
                 }
-
-                block.SetPosition(previousBlock);
             }
         }
 
+        private EnvironmentBlock _lastBlock;
+
         private void Update()
         {
-            
+            if (!_update)
+                return;
+
+            foreach (EnvironmentBlock block in _blocks)
+            {
+                if (block == null)
+                    continue;
+
+                if (block.Position.x > _limit)
+                {
+                    block.Move(_speed);
+                }
+                else
+                {
+                    block.SetPosition(_lastBlock);
+                    _lastBlock.Next = block;
+                    _lastBlock = block;
+                    _lastBlock.Next = null;
+                }
+            }
         }
     } 
 }
